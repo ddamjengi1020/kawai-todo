420294455166091d6e116a76e0da120ef1b94829
ddamjengi <silluat11@gmail.com>
2020-05-05 19:46:20 +0900

    Initial Commit & coding UI part 1

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  StatusBar,
+  TextInput,
+  Dimensions,
+  Platform,
+} from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle="light-content" />
+      <Text style={styles.title}>Kawai To Do</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To Do"} />
+      </View>
     </View>
   );
 }
@@ -12,8 +26,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#F23657",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "white",
+    fontSize: 35,
+    marginTop: 40,
+    marginBottom: 30,
+    fontWeight: "200",
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width - 25,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    ...Platform.select({
+      ios: {
+        shadowColor: "black",
+        shadowOpacity: 0.5,
+        shadowRadius: 10,
+        shadowOffset: {
+          width: 0,
+          height: -1,
+        },
+      },
+      android: {
+        elevation: 8,
+      },
+    }),
   },
 });
---
+++ README.md
@@ -0,0 +1,3 @@
+# *Kawai To Do*
+
+_Kawai To Do app made with React Native_
